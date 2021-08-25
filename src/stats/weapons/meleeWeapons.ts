15da295747da5526e4e5e547aac7e57b3323c9b0
Matthijs Hakfoort <matthijs.hakfoort@rotra.nl>
2021-08-25 11:47:53 +0200

    Cleanup

--- src/stats/weapons/meleeWeapons.ts
+++ src/stats/weapons/meleeWeapons.ts
@@ -126,17 +126,6 @@ export const meleeWeapons: Weapon[] = [
     type: 'MELEE',
   },
   {
-    name: 'Power maul',
-    profile: 'NOSTUN',
-    attackDice: 4,
-    damage: 4,
-    damageCritical: 5,
-    specialRules: [],
-    criticalRules: [],
-    equipment: [],
-    type: 'MELEE',
-  },
-  {
     name: 'Power pick',
     profile: '',
     attackDice: 5,
